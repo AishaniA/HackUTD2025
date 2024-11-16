71706e972b16a6ee1f717756099b96cf7c2c466d
soujanyakam <soujanya.kam5@gmail.com>
2024-11-16 14:40:14 -0600

    navbar code 1

--- hackutd2024/src/components/Navbar.jsx
+++ hackutd2024/src/components/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+
 
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
-}
+return(
+<div>
+<div>
+    {/*Content*/}
+    <div className = "flex items-center justify-between px-8 py-4">
+        <h1 className = "poppins-bold text-3xl text-amber-300">UTDFreebies</h1>
+    {/*Nav*/}
+    <div/>
+    <div className = "poppins-semibold flex gap-5 px-2 py-1 text-2xl">
+    <p>new</p>
+    <p>browse</p>
+    <p>upload</p>
+</div>
+{/*icons*/}
 
-export default Navbar
+
+</div>
+</div>
+
+<hr className = "w-full border t-3 border-gray-400 border opacity-50"></hr>
+</div>
+);
+};
+export default Navbar;
